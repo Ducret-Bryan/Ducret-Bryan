bf9b155618e72a4e047ef3d9d42c3872d5ad39ab
Ducret Bryan <ducret.bryan@gmail.com>
2024-04-10 17:08:04 +0200

    feat: add Realisation, CardProject and add react-device-detect

---
+++ my-app/src/pages/Realisation/data.jsx
@@ -0,0 +1,38 @@
+import project_1 from '../../assets/images/project/test.jpg'
+
+export const dataFilter = ['all', 'application', 'professionnel', 'personnel']
+
+export const dataProject = [
+    {
+        'name': 'Projet n°1',
+        'image': project_1,
+        'description': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        'tags': ['React', 'PHP'],
+        'filter_tag': ['all', 'professionnel'],
+        'url': '/'
+    },
+    {
+        'name': 'Projet n°2',
+        'image': project_1,
+        'description': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        'tags': ['Symfony', 'React', 'PHP'],
+        'filter_tag': ['all', 'application'],
+        'url': '/'
+    },
+    {
+        'name': 'Projet n°3',
+        'image': project_1,
+        'description': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        'tags': ['Symfony', 'React', 'PHP'],
+        'filter_tag': ['all', 'professionnel'],
+        'url': '/'
+    },
+    {
+        'name': 'Projet n°4',
+        'image': project_1,
+        'description': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        'tags': ['Symfony', 'PHP'],
+        'filter_tag': ['all', 'personnel'],
+        'url': '/'
+    },
+]
---
+++ my-app/src/pages/Realisation/index.jsx
@@ -0,0 +1,53 @@
+import { RealisationMain, ClientList, Filter, ButtonFilter, SelectFilter, ProjectGalery } from './style'
+import MainTitle from '../../components/MainTitle'
+import SecondTitle from '../../components/SecondTitle'
+import CardProject from '../../components/CardProject'
+import { dataFilter, dataProject } from './data'
+import { useState } from 'react'
+import { breakPoints } from '../../utils/_variables'
+
+function Realisation() {
+    const [filter, setFilter] = useState('')
+    const isMobile = window.innerWidth < breakPoints.tablet_landscape;
+
+
+    console.log(dataFilter)
+    return (
+        <RealisationMain>
+            <MainTitle label="Mes Réalisations" />
+            <div className="container">
+                <div>
+                    <SecondTitle label={'Client'} />
+                    <ClientList>
+                        <li>Épicerie Thouvenot</li>
+                        <li>Épicerie Thouvenot</li>
+                        <li>Épicerie Thouvenot</li>
+                        <li>Épicerie Thouvenot</li>
+                        <li>Épicerie Thouvenot</li>
+                    </ClientList>
+                </div>
+                <div>
+                    <SecondTitle label={'Galerie'} />
+                    <Filter>
+                        {!isMobile ? dataFilter.map((data) => (
+                            <ButtonFilter onClick={() => setFilter(data)}>{data.slice(0, 1).toUpperCase() + data.slice(1)}</ButtonFilter>
+                        )) :
+                            <SelectFilter onChange={(e) => setFilter(e.target.value)}>
+                                {dataFilter.map((data) => (
+                                    <option value={data}>{data.slice(0, 1).toUpperCase() + data.slice(1)}</option>
+                                ))}
+                            </SelectFilter>
+                        }
+                    </Filter>
+                    <ProjectGalery>
+                        {dataProject.map((project, index) => (
+                            !filter || project.filter_tag.includes(filter)) ? (<CardProject key={index} project={project} />) : null
+                        )}
+                    </ProjectGalery>
+                </div>
+            </div>
+        </RealisationMain>
+    )
+}
+
+export default Realisation
