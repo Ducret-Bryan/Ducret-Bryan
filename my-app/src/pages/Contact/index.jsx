5c8c794ae57aaa54b664aad2c728146fbd1ecf4f
Ducret Bryan <ducret.bryan@gmail.com>
2024-04-25 11:19:41 +0200

    feat: add sendMail.service and emailJs

--- my-app/src/pages/Contact/index.jsx
+++ my-app/src/pages/Contact/index.jsx
@@ -1,14 +1,29 @@
 import MainTitle from "../../components/MainTitle";
-import { MainContact, ContactCard, Card, Icon, Title, Infos, ContactMe, ContactForm, InputContainer, Left, Right, ButtonForm, Submit, Reset } from './style';
+import { MainContact, ContactCard, Card, Icon, Title, Infos, ContactMe, ContactForm, InputContainer, Left, Right, ButtonForm, ErrorMessage, Submit, Reset } from './style';
+import SendMail from '../../utils/service/sendEmail.service'
 
 //Image 
 import iphone from '../../assets/images/icons/contact/iphone.png';
 import placeHolder from '../../assets/images/icons/contact/placeholder.png';
 import email from '../../assets/images/icons/contact/email.png';
 import Separator from "../../components/Separator/";
-
+import { useState } from "react";
 
 function Contact() {
+    const [isEmpty, setIsEmpty] = useState(false);
+
+    // Méthode
+    const SubmitFom = async (e) => {
+        e.preventDefault();
+        const form = e.target;
+        let inputs = form.querySelectorAll('input');
+        inputs.forEach(input => {
+            if (!input.value) setIsEmpty(true);
+        });
+        if (!form.querySelector('textarea').value) setIsEmpty(true);
+
+        if (!isEmpty) SendMail(form);
+    }
 
     return (
         <MainContact>
@@ -40,33 +55,34 @@ function Contact() {
                 <ContactMe>
                     <h2>Contactez-moi</h2>
                     <Separator />
-                    <ContactForm action="" autoComplete="off">
+                    <ContactForm autoComplete="off" onSubmit={SubmitFom}>
                         <InputContainer>
                             <Left>
                                 <div>
-                                    <label htmlFor="name">NOM Prénom</label>
-                                    <input type="text" name="name" placeholder="Dupont Jean" />
+                                    <label htmlFor="from_name">NOM Prénom</label>
+                                    <input type="text" name="from_name" placeholder="Dupont Jean" required />
                                 </div>
                                 <div>
-                                    <label htmlFor="email">E-mail</label>
-                                    <input type="text" name="email" placeholder="dupont.jean@gmail.com" />
+                                    <label htmlFor="from_email">E-mail</label>
+                                    <input type="email" name="from_email" placeholder="dupont.jean@gmail.com" />
                                 </div>
                                 <div>
-                                    <label htmlFor="objet">Objet</label>
-                                    <select name="objet">
-                                        <option value="question_divers" defaultChecked>Question divers</option>
-                                        <option value="question_project">Question sur un projet</option>
-                                        <option value="proposition">Proposition de projet</option>
-                                        <option value="collab">Collaboration</option>
+                                    <label htmlFor="from_subject">Subject</label>
+                                    <select name="from_subject" >
+                                        <option value="Question Divers" defaultChecked>Question divers</option>
+                                        <option value="Question Projet">Question sur un projet</option>
+                                        <option value="Proposition">Proposition de projet</option>
+                                        <option value="Collaboration">Collaboration</option>
                                     </select>
                                 </div>
                             </Left>
                             <Right>
                                 <label htmlFor="message">Message</label>
                                 <textarea name="message" cols="30" rows="10" placeholder="Un jeune homme se réveil dans son lit, plein d'énergie et de projets pour la journée. Il se prépare un café et s'installe à son bureau pour commencer à travailler. Il est déterminé à réussir et à réaliser ses rêves.
-                                "></textarea>
+                                " required></textarea>
                             </Right>
                         </InputContainer>
+                        {isEmpty ? (<ErrorMessage>Les champs du formulaire ne peux pas être vide.</ErrorMessage>) : null}
                         <ButtonForm>
                             <Submit type="submit">Envoyer</Submit>
                             <Reset type="reset">Reset</Reset>
